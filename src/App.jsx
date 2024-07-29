6697eae05159395e7b7c97bdf4198fdecb739639
j10 <jasontenczar@gmail.com>
2024-07-29 18:03:06 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { GoogleGeminiEffectDemo } from './components/GoogleGeminiEffectDemo.tsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleGeminiEffectDemo className='-mt-96 -mb-96'/>
+    
+    </div>
+  );
+}
+
+export default App;
